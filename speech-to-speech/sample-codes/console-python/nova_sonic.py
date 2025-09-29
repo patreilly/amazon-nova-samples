07a185459cf1a4302ac2944c2fb11d9c9d037278
jonathan343 <jonathangee09@gmail.com>
2025-09-29 14:24:44 -0400

    Fix speech-to-speech/sample-codes python examples

--- speech-to-speech/sample-codes/console-python/nova_sonic.py
+++ speech-to-speech/sample-codes/console-python/nova_sonic.py
@@ -15,7 +15,7 @@ from rx.scheduler.eventloop import AsyncIOScheduler
 from aws_sdk_bedrock_runtime.client import BedrockRuntimeClient, InvokeModelWithBidirectionalStreamOperationInput
 from aws_sdk_bedrock_runtime.models import InvokeModelWithBidirectionalStreamInputChunk, BidirectionalInputPayloadPart
 from aws_sdk_bedrock_runtime.config import Config, HTTPAuthSchemeResolver, SigV4AuthScheme
-from smithy_aws_core.credentials_resolvers.environment import EnvironmentCredentialsResolver
+from smithy_aws_core.identity.environment import EnvironmentCredentialsResolver
 
 # Suppress warnings
 warnings.filterwarnings("ignore")
@@ -204,8 +204,6 @@ class BedrockStreamManager:
             endpoint_uri=f"https://bedrock-runtime.{self.region}.amazonaws.com",
             region=self.region,
             aws_credentials_identity_resolver=EnvironmentCredentialsResolver(),
-            http_auth_scheme_resolver=HTTPAuthSchemeResolver(),
-            http_auth_schemes={"aws.auth#sigv4": SigV4AuthScheme()}
         )
         self.bedrock_client = BedrockRuntimeClient(config=config)
     
